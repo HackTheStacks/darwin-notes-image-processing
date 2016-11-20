aa8b3919d9567d59377aa45c19e73a8c14c1b640
Jin Chung <jinchung@ebay.com>
2016-11-20 10:24:31 -0500

    faster image exec

---
+++ Scripts/multi_process_files.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python
+from joblib import Parallel, delayed
+import multiprocessing
+
+import os
+from subprocess import call
+
+inputpath = '/data/amnh/darwin/images'
+segment_exe = '/home/luis_ibanez/bin/darwin-notes-image-processing/Release/Segmentation/ImageToEdges'
+
+def handle_file(filename):
+    call([segment_exe, filename])
+    
+inputs =  os.listdir(inputpath)
+num_cores = multiprocessing.cpu_count()
+results = Parallel(n_jobs=num_cores)(delayed(handle_file)(i) for i in inputs)
+
