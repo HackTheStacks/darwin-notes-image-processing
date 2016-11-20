499792cd917baf7d90798577157ffedaf493643c
Luis Ibanez <luis.ibanez@gmail.com>
2016-11-20 06:02:04 -0500

    Integrate connected components.

---
+++ Segmentation/ImageToEdges.cxx
@@ -0,0 +1,116 @@
+/*=========================================================================
+ *
+ *  Copyright Insight Software Consortium
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *         http://www.apache.org/licenses/LICENSE-2.0.txt
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ *
+ *=========================================================================*/
+
+#include "itkOtsuThresholdImageFilter.h"
+#include "itkImage.h"
+#include "itkImageFileReader.h"
+#include "itkImageFileWriter.h"
+#include "itkBinaryThresholdImageFilter.h"
+#include "itkConnectedComponentImageFilter.h"
+#include "itkRelabelComponentImageFilter.h"
+#include "itkRescaleIntensityImageFilter.h"
+
+int main( int argc, char * argv[] )
+{
+  if( argc < 5 )
+    {
+    std::cerr << "Usage: " << argv[0];
+    std::cerr << " inputImageFile outputImageFile ";
+    std::cerr << " insideValue    outsideValue   "  << std::endl;
+    return EXIT_FAILURE;
+    }
+
+  typedef  unsigned char  InputPixelType;
+  typedef  unsigned char  OutputPixelType;
+  const unsigned int      Dimension = 2;
+
+  typedef itk::Image< InputPixelType,  Dimension >   InputImageType;
+  typedef itk::Image< OutputPixelType, Dimension >   OutputImageType;
+
+  typedef itk::OtsuThresholdImageFilter<
+               InputImageType, OutputImageType >  OtsuFilterType;
+
+  typedef itk::ImageFileReader< InputImageType >  ReaderType;
+  typedef itk::ImageFileWriter< OutputImageType >  WriterType;
+
+  ReaderType::Pointer reader = ReaderType::New();
+  OtsuFilterType::Pointer otsuFilter = OtsuFilterType::New();
+  reader->SetFileName( argv[1] );
+
+  otsuFilter->SetInput( reader->GetOutput() );
+
+  const OutputPixelType outsideValue = atoi( argv[3] );
+  const OutputPixelType insideValue  = atoi( argv[4] );
+
+  otsuFilter->SetOutsideValue( outsideValue );
+  otsuFilter->SetInsideValue(  insideValue  );
+
+  try
+    {
+    otsuFilter->Update();
+    }
+  catch( itk::ExceptionObject & excp )
+    {
+    std::cerr << "Exception thrown " << excp << std::endl;
+    }
+
+  int threshold = otsuFilter->GetThreshold();
+  std::cout << "Threshold = " << threshold << std::endl;
+
+  typedef unsigned short ComponentPixelType;
+  typedef itk::Image< ComponentPixelType, Dimension >  ComponentImageType;
+
+  typedef itk::ConnectedComponentImageFilter <InputImageType, ComponentImageType >
+    ConnectedComponentImageFilterType;
+
+  ConnectedComponentImageFilterType::Pointer connected = ConnectedComponentImageFilterType::New ();
+  connected->SetInput(otsuFilter->GetOutput());
+  connected->Update();
+
+  std::cout << "Number of objects: " << connected->GetObjectCount() << std::endl;
+
+  typedef itk::RelabelComponentImageFilter<
+               ComponentImageType, ComponentImageType >  RelabelFilterType;
+  RelabelFilterType::Pointer relabeler = RelabelFilterType::New();
+
+  relabeler->SetInput( connected->GetOutput() );
+
+  typedef itk::BinaryThresholdImageFilter<
+               ComponentImageType, OutputImageType >  ThresholdFilterType;
+  ThresholdFilterType::Pointer thresholder = ThresholdFilterType::New();
+  thresholder->SetLowerThreshold(1);
+  thresholder->SetUpperThreshold(1);
+  thresholder->SetOutsideValue(0);
+  thresholder->SetInsideValue(255);
+
+  thresholder->SetInput( relabeler->GetOutput() );
+
+  WriterType::Pointer writer = WriterType::New();
+  writer->SetFileName( argv[2] );
+  writer->SetInput( thresholder->GetOutput() );
+  writer->Update();
+
+  std::cout << "Largest object of " << connected->GetObjectCount() << " objects";
+
+  typedef std::vector< itk::SizeValueType > SizesInPixelsType;
+  const SizesInPixelsType & sizesInPixels = relabeler->GetSizeOfObjectsInPixels();
+  std::cout << "Number of pixels in largest component = " << sizesInPixels[0] << std::endl;
+  std::cout << "Number of pixels in second  component = " << sizesInPixels[1] << std::endl;
+
+  return EXIT_SUCCESS;
+}
