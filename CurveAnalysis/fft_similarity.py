7c45478faf188c7fe2664175109b19b3ad550038
jinchung <jin.chung17@gmail.com>
2016-12-04 17:39:48 -0500

    similarity scores against all other curve ffts and sort

--- CurveAnalysis/fft_similarity.py
+++ CurveAnalysis/fft_similarity.py
@@ -2,10 +2,15 @@
 import numpy as np
 import sys
 import scipy.io as sio
+import os
+import operator
 
-base_dir = '/home/queenjin/sandbox/amnh/matchural/'
-#base_dir = '/data/amnh/darwin/'
+#base_dir = '/home/queenjin/sandbox/amnh/matchural/'
+base_dir = '/data/amnh/darwin/'
 curves_fft_dir = base_dir + 'image_csvs_fft/'
+fft_similarity_dir = base_dir + 'fft_similarity/'
+
+sim_score_map = {}
 
 def similarity_score(curve1_filename, curve2_filename):
     curve1_name = curves_fft_dir + curve1_filename 
@@ -16,19 +21,28 @@ def similarity_score(curve1_filename, curve2_filename):
 
     norm1 = np.linalg.norm(fft1)
     norm2 = np.linalg.norm(fft2)
-    print(norm1)
-    print(norm2)
-    print('\ndot product')
     dot_product = np.absolute(np.vdot(fft1, fft2))
-    print(dot_product)
     mul_norm = norm1 * norm2
     if mul_norm != 0: 
         return dot_product / mul_norm 
     return 0
 
+def stringify(x):
+    float_score = '%.20f' % x[1]
+    return x[0] + '\t' + float_score
+    
 curve1_filename = sys.argv[1]
-curve2_filename = sys.argv[2]
+input_files = os.listdir(curves_fft_dir)
+fft_similarity_filename = fft_similarity_dir + curve1_filename[:-4] + '_similarity.txt'
 
-sim_score = similarity_score(curve1_filename, curve2_filename)
-print('\nsim score:')
-print(sim_score)
+for curve2_filename in input_files:
+    sim_score = similarity_score(curve1_filename, curve2_filename)
+    sim_score_map[curve2_filename] = sim_score 
+
+# sort
+final_sorted_results = sorted(sim_score_map.items(), key=operator.itemgetter(1), reverse=True)
+fft_sim_file = open(fft_similarity_filename, 'w')
+stringified_results = [stringify(x) for x in final_sorted_results]
+fft_sim_file.write('\n'.join(stringified_results))
+fft_sim_file.close()
+
