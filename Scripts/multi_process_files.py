bb0740aaa33c1fdb492e88a5d15b08727a82f4b4
Luis Ibanez <luis.ibanez@gmail.com>
2017-01-15 11:53:45 -0500

    Restore directories to data and exe as exist in the cloud vm.

--- Scripts/multi_process_files.py
+++ Scripts/multi_process_files.py
@@ -5,10 +5,8 @@ import multiprocessing
 import os
 from subprocess import call
 
-# inputpath = '/data/amnh/darwin/images'
-# segment_exe = '/home/luis_ibanez/bin/darwin-notes-image-processing/Release/Segmentation/ImageToEdges'
-inputpath = '/home/ibanez/data/amnh/darwin_notes/images'
-segment_exe = '/home/ibanez/bin/amnh/darwin/darwin-notes-image-processing/Release/Segmentation/ImageAnalyzer'
+inputpath = '/data/amnh/darwin/images'
+segment_exe = '/home/luis_ibanez/bin/darwin-notes-image-processing/Release/Segmentation/ImageAnalyzer'
 
 def handle_file(filename):
     call([segment_exe, filename])
